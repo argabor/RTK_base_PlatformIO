4395407e06fc3c18cbbdf1e746c8e432778d19c3
Gabor Arvai <argabor@gmail.com>
2023-02-15 21:20:01 +0100

    comments

--- include/credentials_sample.h
+++ include/credentials_sample.h
@@ -4,7 +4,7 @@
 
 // NTrip Caster Settings
 #define HOST "rtk2go.com"
-#define HTTP_PORT 2101
+#define HTTP_PORT 2101 //port 2101 is default port of NTRIP caster
 #define MNT_PNT "YourNTRIPMountPoint"
 #define PSW "YourNTRIPPassword"
-#define SRC_STR ""
+#define SRC_STR "" //source table infomation string reference software.rtcm-ntrip.org/wiki : NTRIP sourcetable contents (https://software.rtcm-ntrip.org/wiki/Sourcetable)
